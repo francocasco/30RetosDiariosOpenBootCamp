131349426e060ad19aaa6d50a5ff1349972f9ec2
Franco Casco <cascof.it@gmail.com>
2023-01-27 17:14:40 -0300

    Recibe una url y ejecuta el script

--- ScriptExcel/ScriptExcel.py
+++ ScriptExcel/ScriptExcel.py
@@ -1,15 +1,16 @@
-# librerias
-# pip install numpy
-# pip install pandas
-# pip install openpyxl
-
 #importar librerias
 import numpy as np
 import pandas as pd
 
+from io import open
+
+print("Escriba la direccion del archivo excel")
+url=input()
+print(url)
+
 #lectura del archivo xlsx
 
-notas_df=pd.read_excel('nombres.xlsx')
+notas_df=pd.read_excel(url)
 
 # Mostrar el contenido del libro1
 print(notas_df[['Email']])
